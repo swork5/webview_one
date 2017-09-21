a1f4c8e34fd38e2af5270de39514eb7eb9a57b8b
swork5 <work.samp05@gmail.com>
2017-09-21 14:43:57 +0000

    Dynamo Error Fix

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const
   express = require('express'),
   https = require('https'),  
   request = require('request');
-  AWS=require('aws-sdk');
+  //AWS=require('aws-sdk');
 
 var app = express();
 app.set('port', process.env.PORT || 5000);
@@ -221,6 +221,7 @@ function receivedMessage(event) {
   var recipientID = event.recipient.id;
   var timeOfMessage = event.timestamp;
   var message = event.message;
+  var docClient = new AWS.DynamoDB.DocumentClient();
 
   const dynamoParams = {
     TableName: "Logs",
@@ -232,7 +233,7 @@ function receivedMessage(event) {
     },
   };
 
-  dynamoDb.put(dynamoParams, (error) => {
+  docClient.put(dynamoParams, (error) => {
       if (error) {
         console.error(error);
       }
